4df08dc1c43dc94a220e241c12e9a8bf5b826e4a
acsbendi <acsbendi28@gmail.com>
2019-12-07 19:35:26 +0100

    Fixed mapping String[] to the database schema.

--- src/main/java/com/bendeguz/biddingapp/core/Campaign.java
+++ src/main/java/com/bendeguz/biddingapp/core/Campaign.java
@@ -3,7 +3,9 @@ package com.bendeguz.biddingapp.core;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "campaigns")
@@ -23,9 +25,30 @@ public class Campaign {
     @NotNull
     private String name;
 
-    @Column(name = "keywords", nullable = false)
+    /**
+     *  Perhaps it's not the most obvious solution to use a Set but everything else failed to work:
+     *
+     *  - When I tried to map the field to h2's built-in array type, the following exception occurred:
+     *      org.h2.jdbc.JdbcSQLException: Hexadecimal string contains non-hex character
+     *    As I could not find any sources online regarding this mapping, I concluded this is not possible.
+     *
+     *  - When I tried to use Array[] with ElementCollection and CollectionTable, the following exception occurred:
+     *      org.hibernate.AnnotationException: List/array has to be annotated with an @OrderColumn
+     *    As far as I could find out, this means that I should create another column just for the ordering.
+     *    I think this solution would have been more hacky, so I went with using a Set type instead.
+     *
+     *  I also think that it makes sense to use a set, as a duplicate keyword wouldn't add any value anyway.
+     *  This is why I decided to use a Set instead of a List.
+     *
+     */
+    @ElementCollection
+    @CollectionTable(
+            name = "keywords",
+            joinColumns = @JoinColumn(name = "campaign_id")
+    )
+    @Column(name = "keyword", nullable = false)
     @NotNull
-    private String[] keywords;
+    private Set<String> keywords = new HashSet<>();
 
     @Column(name = "budget", nullable = false)
     private double budget;
@@ -36,7 +59,7 @@ public class Campaign {
 
     public Campaign(String name, String[] keywords, double budget) {
         this.name = name;
-        this.keywords = keywords;
+        this.keywords.addAll(Arrays.asList(keywords));
         this.budget = budget;
     }
 
@@ -56,11 +79,11 @@ public class Campaign {
         this.name = name;
     }
 
-    public String[] getKeywords(){
+    public Set<String> getKeywords(){
         return keywords;
     }
 
-    public void setKeywords(String[] keywords) {
+    public void setKeywords(Set<String> keywords) {
         this.keywords = keywords;
     }
 
@@ -86,11 +109,11 @@ public class Campaign {
         return Objects.equals(this.id, that.id) &&
                 Objects.equals(this.name, that.name) &&
                 Objects.equals(this.budget, that.budget) &&
-                Arrays.equals(this.keywords, that.keywords);
+                Arrays.equals(this.keywords.toArray(), that.keywords.toArray());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, budget, Arrays.hashCode(keywords));
+        return Objects.hash(id, name, budget, Arrays.hashCode(keywords.toArray()));
     }
 }
