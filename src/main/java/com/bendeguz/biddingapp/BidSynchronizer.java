93c855d43b6cc04daef8139292a512aa68f7e399
acsbendi <acsbendi28@gmail.com>
2019-12-08 22:03:49 +0100

    Use code styling in Javadoc for BidSynchronizer methods.

--- src/main/java/com/bendeguz/biddingapp/BidSynchronizer.java
+++ src/main/java/com/bendeguz/biddingapp/BidSynchronizer.java
@@ -12,10 +12,10 @@ import java.util.concurrent.Semaphore;
  * This class provides mechanisms to check whether a campaign is available for bidding and to handle concurrency between bids.
  * To ensure that no more spending than {@code MAXIMUM_SPENDING_PER_CAMPAIGN_PER_10_SEC} occurs per 10 sec,
  * the following methods needs to be called in this order:
- * 1. lockCampaign - to lock the campaign so no other concurrent request can access it
- * 2. isCampaignAvailable - to check if the previously mentioned limit is not exceeded
- * 3. spendOnCampaign - if the caller will attempt to spend on the campaign (this attempt will probably succeed).
- * 4. unlockCampaign - it is <b>crucial</b> to make sure this method gets called, because otherwise the campaign
+ * 1. {@code lockCampaign} - to lock the campaign so no other concurrent request can access it
+ * 2. {@code isCampaignAvailable} - to check if the previously mentioned limit is not exceeded
+ * 3. {@code spendOnCampaign} - if the caller will attempt to spend on the campaign (this attempt will probably succeed).
+ * 4. {@code unlockCampaign} - it is <b>crucial</b> to make sure this method gets called, because otherwise the campaign
  *                     will become unavailable for bidding until the application is restarted.
  */
 public class BidSynchronizer {
