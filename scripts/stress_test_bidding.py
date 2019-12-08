30e29cf85cf101582a87ade96cb64fc1980d48d4
acsbendi <acsbendi28@gmail.com>
2019-12-08 14:06:23 +0100

    Created Python script to stress test bidding.

---
+++ scripts/stress_test_bidding.py
@@ -0,0 +1,17 @@
+import requests
+import threading
+import time
+import functools
+
+NUMBER_OF_BIDDING_TO_CREATE = 10
+
+def create_bidding(i):
+    data = { "bidId": i, "keywords": ["Kobler"]}
+    url ="http://localhost:8080/bids"
+    response = requests.post(url, json=data)
+    print(f"{response} - {response.elapsed.total_seconds()}")
+
+for i in range(0, NUMBER_OF_BIDDING_TO_CREATE):
+    time.sleep(0.01)
+    t = threading.Thread(target=functools.partial(create_bidding, i + 1))
+    t.start()
